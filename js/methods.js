c80bbddc16ccbcbf6059168ea8bea93c76e0f991
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-19 19:00:28 -0500

    solution

---
+++ js/methods.js
@@ -0,0 +1,27 @@
+
+
+function fancySchmancy(){
+  $('#bacon').addClass("iAmFancy");
+  return $('.iAmFancy');
+}
+
+function addText(sentence){
+  $('p:first-child + p').append(sentence);
+  return $('p:first-child + p');
+}
+
+function showDolphin(){
+  $('#hidden').show();
+}
+
+function hideImage(){
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt(){
+  $("#favorite-snacks").fadeIn();
+}
+
+function fadeItOut(){
+  $('p:first-child + p').fadeOut();
+}
