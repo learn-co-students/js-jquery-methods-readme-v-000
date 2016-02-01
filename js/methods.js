0662752ba0746d875a14169abe70486b51aa8ab5
Lukeghenco <lukeghenco@gmail.com>
2016-02-01 00:14:56 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,45 @@
 'use strict';
+
+$(document).ready(function() {
+
+  fancySchmancy();
+  addText("monkey butt");
+  showDolphin();
+  fadeIt(3000);
+
+
+});
+
+function fancySchmancy() {
+  $('p').addClass("iAmFancy");
+  return $('p');
+}
+
+function addText(string) {
+  $('p:nth-child(2)').append(string);
+  return $('p:nth-child(2)');
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt(speed) {
+  $('div#favorite-snacks').fadeIn(speed);
+}
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('input:last').val();
+}
