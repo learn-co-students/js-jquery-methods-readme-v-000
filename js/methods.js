04cc09c96aabfc1f266e3674e4266ffc91c97fc6
macgeek30 <mike25ko@gmail.com>
2016-02-03 15:34:16 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,36 @@
 'use strict';
+
+
+function fancySchmancy() {
+  $("p#bacon").addClass("iAmFancy");
+  return $('.iAmFancy');
+}
+
+function addText(string) {
+  return $('p:nth-child(2)').append(string);
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last-child').val();
+}
+
