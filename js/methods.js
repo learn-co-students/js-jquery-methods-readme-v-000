d5fe658ce239fcb6a7345d56662fe49d408158b8
Karuna <imkaruna@gmail.com>
2016-02-08 20:05:06 -0500

    Done

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function fancySchmancy() {
+  $('#bacon').addClass('iAmFancy');
+  return $('#bacon');
+}
+
+function addText(stringValue){
+  $('p:nth-child(2)').append(stringValue);
+  return $('p:nth-child(2)');
+}
+
+function showDolphin() {
+  $('#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+
+}
+
+function findItByClass(string) {
+  return $(string).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('input:last').val();
+}
