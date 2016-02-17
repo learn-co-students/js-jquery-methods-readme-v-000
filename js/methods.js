9328e84c5a92177b0fd471ca852b942a80042974
selichter <lichter.sarahe@gmail.com>
2016-02-17 10:18:50 -0600

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy(){
+  return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(){
+  return $('p:nth-child(2)').append("string to add");
+}
+
+function showDolphin(){
+  $('img#hidden').show();
+}
+
+function hideImage(){
+  $('[alt="pie in face"]').hide();
+}
+
+function fadeIt(){
+  $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut(){
+  $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(selector){
+  return $(selector).hasClass("flatironLink");
+}
+
+function formValue(){
+  return $('input:last').val();
+}
