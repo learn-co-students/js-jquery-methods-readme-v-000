455549ec7208b7be4b15bf8d75cbfc78f6ff34b8
Shana L Moore <shana.lavina.moore@gmail.com>
2016-03-02 12:10:47 -0800

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy () {
+  return $('p#bacon').addClass("iAmFancy");
+}
+
+function addText (string) {
+  return $('body p:nth-child(2)').append(string);
+}
+
+function showDolphin () {
+  $('img#hidden').show();
+}
+
+function hideImage () {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt () {
+  $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut () {
+  $('body p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass (selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue () {
+  return $('input:last').val();
+}
