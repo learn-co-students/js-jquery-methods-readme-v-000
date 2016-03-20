d2eccea64de4b8ab3eb6eb2e87d75415eae60f8f
Adam King <adam.king0126@gmail.com>
2016-03-20 07:51:28 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('#bacon').addClass('iAmFancy');
+}
+
+function addText(text) {
+  return $('p:nth-child(2)').append(text);
+}
+
+function showDolphin() {
+  $('#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  return $('#favorite-snacks').fadeIn(2000);
+}
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('input:last').val();
+}
+
+
