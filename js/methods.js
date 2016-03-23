35645886edb9e0158c9757216b1f9dbf0a61243c
Alvinluipo <lualvin1234@gmail.com>
2016-03-22 23:03:30 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  $('p').addClass("iAmFancy");
+  return $('p');
+}
+
+function addText(string) {
+  $('p:nth-child(2)').append(string);
+  return $('p:nth-child(2)');
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt(speed) {
+  $('div#favorite-snacks').fadeIn(speed);
+}
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('input:last').val();
+}
