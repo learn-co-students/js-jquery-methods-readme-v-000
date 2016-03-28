b63b457c56184bfc5f96a0633e405711b84e9e24
Rawradin <edgar.rawr@gmail.com>
2016-03-28 15:29:37 -0700

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function fancySchmancy(){
+  $('#bacon').addClass('iAmFancy');
+  return($('#bacon'));
+}
+
+function addText(string){
+  $('p:first-child + p').append(string);
+  return $('p:first-child + p');
+}
+
+function showDolphin(){
+  $('#hidden').show();
+}
+
+function hideImage(){
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt(){
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut(){
+  $('p:first-child + p').fadeOut(0);
+  return $('p:first-child + p');
+}
+
+function findItByClass(string){
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue(){
+  return $('input:last').val();
+}
