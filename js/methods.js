3c4c0d96195525aef76c0bd4bc3e24d8cae6d8ff
Minh N <mhnguyen289@gmail.com>
2016-03-28 21:37:25 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy(){
+  return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(string){
+  return $('p:nth-of-type(2)').append(string);
+}
+
+function showDolphin(){
+  $('img#hidden').show();
+}
+
+function hideImage(){
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt(){
+ $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:nth-of-type(2)').fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
