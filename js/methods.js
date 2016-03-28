37ec694d0f761b7e9b9e30615e54cab668601266
Marco Tsui <marcotsui2003@yahoo.com>
2016-03-28 17:57:23 -0400

    done

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p:nth-of-type(2)').append(string);
+}
+
+function showDolphin() {
+  $('#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:first-child + p').fadeOut(0);
+}
+
+function findItByClass(tag) {
+  return tag=='a';
+}
+
+function formValue() {
+  return $('input:last-child').val();
+}
