a167203f9ba31c1aef259dc00e83fdfd446b3f95
NStephenson <nickstephenson90@gmail.com>
2016-04-06 07:21:27 -0400

    made all tests pass

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,36 @@
 'use strict';
+function fancySchmancy(){
+  return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(text){
+  return $('p:last').append(text);
+}
+
+function showDolphin(){
+  $("img#hidden").show();
+}
+
+function hideImage(){
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt(){
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut(){
+  $('p:last').fadeOut(0);
+}
+
+function findItByClass(selector){
+  return $(selector).hasClass('flatironLink');
+}
+
+function fadeItOut(){
+  $('p:last').fadeOut(0);
+}
+
+function formValue(){
+  return $('input:last').val()
+}
