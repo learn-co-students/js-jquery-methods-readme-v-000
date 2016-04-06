6770cb02bbe955c07e63772bfa4989de1784abc4
pacosta613 <pedro.acosta613@gmail.com>
2016-04-06 14:42:00 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy(){
+  return $('p').addClass('iAmFancy')
+}
+
+function addText(string){
+  return $('p:last').append(string);
+}
+
+function showDolphin(){
+  $('img').show();
+}
+
+function hideImage(){
+  $('img').hide();
+}
+
+function fadeIt(){
+  $('div').fadeIn();
+}
+
+function fadeItOut(){
+  $('p:last').fadeOut(0);
+}
+
+function findItByClass(string){
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue(){
+  return $('input:last').val();
+}
