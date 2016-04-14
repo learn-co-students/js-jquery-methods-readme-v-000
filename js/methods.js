310a4945ed8a31ca52da4382e6bc869f5576c011
pickledyamsman <pickledyamsman@gmail.com>
2016-04-14 08:57:16 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p').addClass('iAmFancy');
+}
+
+function addText(str) {
+  return $('p:last').append(str);
+}
+
+function showDolphin() {
+  $('#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:last').fadeOut(0);
+}
+
+function findItByClass(str) {
+  return $(str).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
