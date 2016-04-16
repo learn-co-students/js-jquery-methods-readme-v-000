db89adb35b446e9a3b1555c954c19fa3b373f3ca
febbraiod <donedesign1@gmail.com>
2016-04-16 11:29:22 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy(){
+  $('#bacon').addClass('iAmFancy');
+  return $('.iAmFancy');
+}
+
+function addText(str){
+  $('#bacon').next().append(str);
+  return $('#bacon').next();
+}
+
+function showDolphin(){
+  $('#hidden').show();
+}
+
+function hideImage(){
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt(){
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut(){
+  $('#bacon').next().fadeOut(0);
+}
+
+function findItByClass(str){
+  return $(str).hasClass('flatironLink')
+}
+
+function formValue(){
+  return $('input:last').val();
+}
