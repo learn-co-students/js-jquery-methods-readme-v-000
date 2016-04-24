f1745ce4e98b0b4dff7793058babb657f568ad92
azheng249 <andy.zheng249@gmail.com>
2016-04-23 23:08:38 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,37 @@
 'use strict';
+
+function fancySchmancy () {
+  $("p#bacon").addClass("iAmFancy");
+  return $("p#bacon");
+}
+
+function addText (string) {
+  $("p:eq(1)").append(string);
+  return $("p:eq(1)");
+}
+
+function showDolphin () {
+  $("img#hidden").show();
+  return $("img#hidden");
+}
+
+function hideImage () {
+  $("img[alt='pie in face']").hide();
+  return $("img[alt='pie in face']");
+}
+
+function fadeIt () {
+  $("#favorite-snacks").fadeIn();
+}
+
+function fadeItOut () {
+  $("p:eq(1)").fadeOut(0);
+}
+
+function findItByClass (tag) {
+  return $(tag).hasClass("flatironLink");
+}
+
+function formValue () {
+  return $("input:last").val();
+}
