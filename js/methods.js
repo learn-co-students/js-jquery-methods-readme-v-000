6dbbb1fedb3e34f45d85a5a2ba90fb7eedd87ed8
Dakota Martinez <dakotaleemusic@gmail.com>
2016-04-29 08:14:28 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,32 @@
 'use strict';
+function fancySchmancy() {
+    return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+    return $('p:nth-child(2)').append(string);
+}
+
+function showDolphin() {
+    return $('#hidden').show();
+}
+
+function hideImage() {
+    return $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+    $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+    $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(string) {
+    return $(string).hasClass('flatironLink');
+}
+
+function formValue() {
+    return $("input:last").val();
+}
