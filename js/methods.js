6e950b96e222b91f9fde816280d0a42e6f43aa87
the-widget <brennenawana108ny@gmail.com>
2016-05-02 20:23:52 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  $("p#bacon").addClass("iAmFancy");
+  return $("p#bacon");
+}
+
+function addText(string) {
+  $("p:last").append(string);
+  return $("p:last");
+}
+
+function showDolphin() {
+  $("#hidden").show(); 
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $("div#favorite-snacks").fadeIn(4000);
+}
+
+function fadeItOut() {
+  $("p:last").fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $("input:last").val();
+}
