e93cebd6c80177c947635c3bb461fa6605fd4903
Brian Holland <brian.holland@proatria.com>
2016-05-04 16:34:11 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,37 @@
 'use strict';
+
+function fancySchmancy(){
+  var $p = $('p');
+  $p.addClass('iAmFancy');
+  return $p;
+};
+
+function addText(s){
+  var $p = $('p:last');
+  $p.append(s);
+  return $p;
+};
+
+function showDolphin(){
+  $('#hidden').show();
+};
+
+function hideImage(){
+  $('img[alt="pie in face"]').hide();
+};
+
+function fadeIt(){
+  $('#favorite-snacks').fadeIn(2000);
+};
+
+function fadeItOut(){
+  $('p:first-child + p').fadeOut(0);
+};
+
+function findItByClass(selector_input){
+  return $(selector_input).hasClass("flatironLink");
+};
+
+function formValue(){
+  return $(':input:last').val();
+};
