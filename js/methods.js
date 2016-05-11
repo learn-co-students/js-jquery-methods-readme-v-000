262c1a8457fdc3fc6cd00899fbdd560bcc5973f7
Brad1974 <danieltbrad@gmail.com>
2016-05-11 09:13:42 -0700

    done

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  $('#bacon').addClass("iAmFancy");
+  return $('p');
+}
+
+function addText(text) {
+  $("p:nth-of-type(2)").append(text);
+  return $("p:nth-of-type(2)");
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $("p:nth-of-type(2)").fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
