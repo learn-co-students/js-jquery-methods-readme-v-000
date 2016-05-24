96aa1a25609ded7bfc85c8513947ddfe67e0c553
Andrew Papacostas <a_papacostas@yahoo.com>
2016-05-24 12:18:12 +0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function fancySchmancy(){
+  $('#bacon').addClass("iAmFancy");
+  return $('.iAmFancy');
+}
+
+function addText(sentence){
+  $('p:first-child + p').append(sentence);
+  return $('p:first-child + p');
+}
+
+function showDolphin(){
+  $('#hidden').show();
+}
+
+function hideImage(){
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt(){
+  $("#favorite-snacks").fadeIn();
+}
+
+function fadeItOut(){
+  $('p:first-child + p').fadeOut(0);
+  return $('p:first-child + p');
+}
+
+function findItByClass(selector){
+  return $(selector).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('input:last').val();
+}
