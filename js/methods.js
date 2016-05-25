64e1a1d3ef132e869c4903be19de329c4b6969c2
gormanjp <jpgorman8@gmail.com>
2016-05-24 20:22:23 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy(){
+  $('#bacon').addClass("iAmFancy");
+  return $('.iAmFancy');
+}
+
+function addText(words){
+	$('p:last').append(words)
+	return $('p:last')
+}
+
+function showDolphin(){
+	$('#hidden').show()
+}
+
+function hideImage(){
+	$('img:last').hide()
+}
+
+function fadeItOut(){
+	$('p:last').fadeOut(0)
+}
+
+function findItByClass(select){
+	return $(select).hasClass('flatironLink')
+}
+
+function formValue(){
+	return $('input:last').val()
+}
+
+function fadeIt(){
+	$('#favorite-snacks').fadeIn()
+}
