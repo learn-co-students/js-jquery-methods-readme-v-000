0fad60c233454c6a0f122cf3ba27fae11c13dd28
LKWLaLa <lkwlala@gmail.com>
2016-05-30 16:55:32 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy(){
+  $('#bacon').addClass("iAmFancy");
+  return $('.iAmFancy');
+}
+
+function addText(myString){
+  $('p:first-child + p').append(myString);
+  return $('p:first-child + p');
+}
+
+function showDolphin(){
+  $('#hidden').show();
+}
+
+function hideImage(){
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt(){
+  $('div #favorite-snacks').fadeIn();
+}
+
+function fadeItOut(){
+  $('p:first-child + p').fadeOut(0);
+}
+
+function findItByClass(myString){
+  return $(myString).hasClass("flatironLink");
+}
+
+function formValue(){
+  return $('input:last').val()
+}
