efa9f423031f9eebeb76e8f255dbb9b8a1af869c
McN33ly <rmcneely23@gmail.com>
2016-06-02 00:23:52 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy')
+}
+
+function addText(string) {
+  $('p:last').append(string)
+  return $('p:last')
+}
+
+function showDolphin() {
+  $('img#hidden').show()
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide()
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn()
+}
+
+function fadeItOut() {
+  $('p:last').fadeOut(0)
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink') ? true : false
+}
+
+function formValue() {
+  return $('input:last').val()
+}
