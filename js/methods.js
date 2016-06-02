d123999b0fd66654fbdd7c3f76d8de94ae4e9577
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-06-02 10:39:33 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,27 @@
 'use strict';
+//class .
+//id    #
+function fancySchmancy(){
+  return $('p#bacon').addClass("iAmFancy ");
+}
+
+function addText(string){
+  return $('p:nth-of-type(2)').append(string);
+}
+
+function showDolphin() {
+  //$("img[alt='smiling dolphin 2']").show();
+  $('#hidden').show();
+}
+
+function hideImage(){
+  $('img[alt = "pie in face"]').hide();
+}
+
+function fadeIt(){
+    $('#favorite-snacks').fadeIn(0);
+}
+
+function formValue(){
+  $('input:last-child').val();
+}
