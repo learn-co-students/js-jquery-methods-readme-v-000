a0f88091ccdb5492699a3f54c63d34a29ca846a3
Ziv Zamechek <ziv921@yahoo.com>
2016-06-03 14:30:01 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p:last').append(string);
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn(4000);
+}
+
+function fadeItOut() {
+  return $('p:last').fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
