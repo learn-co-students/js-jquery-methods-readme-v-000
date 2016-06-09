be47b4e20b9452669705bfb70b14d671e64b1b48
John Carlo  Malto <jakemalto2014@gmail.com>
2016-06-09 10:45:25 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+   return $('p#bacon').addClass('iAmFancy');
+ };
+
+ function addText(string) {
+   return $('p:first-child + p').append(string);
+ };
+
+ function showDolphin() {
+   $('img#hidden').show();
+ };
+
+ function hideImage() {
+   $('img[alt="pie in face"]').hide();
+ };
+
+ function fadeIt() {
+   $('div#favorite-snacks').fadeIn(4000);
+ };
+
+ function fadeItOut() {
+   $('p:last').fadeOut(0);
+ };
+
+ function findItByClass(string) {
+   return $(string).hasClass('flatironLink');
+ };
+
+ function formValue() {
+   return $('input:last').val();
+ };
