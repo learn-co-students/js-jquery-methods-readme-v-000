c6123e42df429139bec87d04c830f2b27832822d
Peter Jacobson <github email address>
2016-07-10 15:52:52 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,33 +1,41 @@
 'use strict';
 
 function fancySchmancy(){
-return $('p:#bacon').addClass(“iAmFancy”);
+  $('#bacon').addClass("iAmFancy");
+  return $('.iAmFancy');
 }
 
-function addText(string){
- return $('p:nth-child(2)').append(string);
+function addText(sentence){
+  $('p:first-child + p').append(sentence);
+  return $('p:first-child + p');
 }
 
 function showDolphin(){
-  $('img[alt=‘smiling dolphin 2’]').show();
+  $('#hidden').show();
 }
 
 function hideImage(){
- $('img[alt=‘pie in face’]').hide();
+  $('img[alt="pie in face"]').hide();
 }
 
 function fadeIt(){
-   $('#favoritesnacks').fadeIn();
+  $("#favorite-snacks").fadeIn();
 }
 
 function fadeItOut(){
-   $('p:nth-child(2)').fadeOut(-1000);
+  $('p:first-child + p').fadeOut(0);
+  return $('p:first-child + p');
 }
 
-function findItByClass(string) {
-  return $('a').hasClass("flatironLink");
+function findItByClass(selector){
+  return $(selector).hasClass("flatironLink");
 }
 
-function formValue() {
+function animateIt(){
+  $('#box').animate({height: "500px"});
+}
+
+
+function formValue(){
   return $('input:last').val();
 }
