9aa0d9cbe043b97a7800746290df454c49765efe
michaelsimon <michael.simon@gmail.com>
2016-07-15 19:57:37 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,32 @@
 'use strict';
+function fancySchmancy() {
+  return $("p[id='bacon']").addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p:eq(1)').append(string);
+}
+
+function showDolphin() {
+  $("#hidden").show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $("div[id='favorite-snacks']").fadeIn();
+}
+
+function fadeItOut() {
+  $("p:eq(1)").fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
