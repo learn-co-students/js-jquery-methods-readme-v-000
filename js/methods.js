f789732a13ccc3ddad7ce2b020b7f3d9a641f736
Gandhi Anderson <thegands@gmail.com>
2016-07-23 00:06:30 -0600

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+    return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p + p').append('string')
+}
+
+function showDolphin() {
+  $('#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn(500);
+}
+
+function fadeItOut () {
+  $('p + p').fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
