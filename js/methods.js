723931070c176727913d675a67b12a93c0b2076a
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-08-11 11:11:56 -0700

    done

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,27 @@
 'use strict';
+function fancySchmancy() {
+  $("#bacon").addClass("iAmFancy");
+  return $('.iAmFancy');
+}
+function addText(addString) {
+  $('p:nth-child(2)').appendTo('addString');
+  return $('p:nth-child(2)');
+}
+function showDolphin() {
+  $('#hidden').show();
+}
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+}
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+}
+function findItByClass(jqSelector) {
+  return $(jqSelector).hasClass("flatironLink");
+}
+function formValue() {
+  return $('input:last').val();
+}
