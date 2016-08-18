791b934c2e5a52029a4d2619f487e9bd06ca9ad4
Josh Avina <josh.avina78@gmail.com>
2016-08-17 22:24:21 -0700

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,39 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy');
+};
+
+function addText(string) {
+  return $('p:first-child + p').append(string);
+};
+
+function showDolphin() {
+  $('#hidden').show();
+};
+
+function hideImage(){
+  $('img[alt="pie in face"]').hide();
+};
+
+function fadeIt(){
+  $("#favorite-snacks").fadeIn();
+};
+
+function fadeItOut(){
+  $('p:first-child + p').fadeOut(0);
+  return $('p:first-child + p');
+};
+
+function findItByClass(selector){
+  return $(selector).hasClass("flatironLink");
+};
+
+function animateIt(){
+  $('#box').animate({height: "500px"});
+};
+
+
+function formValue(){
+  return $('input:last').val();
+};
