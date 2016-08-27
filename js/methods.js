870fadae299e4eb4e2e65fb8a34e5c8b81a4c0a2
Devin Burnette <devin.burnette@gmail.com>
2016-08-27 03:37:22 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $("p[id='bacon']").addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p').eq(1).append(string);
+}
+
+function showDolphin() {
+  $('#hidden').show(0);
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide(0);
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn(0);
+}
+
+function fadeItOut() {
+  $('p').eq(1).fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
