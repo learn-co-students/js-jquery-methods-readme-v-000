1415d0b827d847dd02310aaaf4ebff4b5cd75e17
Adam Taitano <adam.taitano@gmail.com>
2016-09-10 18:24:52 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function fancySchmancy() {
+  $('p#bacon').addClass('iAmFancy');
+  return $('.iAmFancy');
+};
+
+function addText(string) {
+  $('p:nth-child(2)').append(string);
+  return $('p:nth-child(2)');
+};
+
+function showDolphin() {
+  $('#hidden').show();
+};
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+};
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+};
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+  return $('p:nth-child(2)');
+};
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+};
+
+function formValue() {
+  return $('input').last().val();
+};
