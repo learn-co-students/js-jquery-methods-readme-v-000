16904fcc6e89d4ae10619714ae733c4a69cb8edf
abadfish <kirsten.ofarrell@gmail.com>
2016-09-11 17:51:30 -0400

    done

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p:nth-child(2)').append(string);
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn(2000);
+}
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(selectorString) {
+  return $(selectorString).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('input:last').val()
+}
