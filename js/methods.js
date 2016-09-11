82cf226b502f0a7f2fcba17e1a20bcfdcb1d3d7a
tttra <tothutra@gmail.com>
2016-09-11 00:01:13 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  $('p#bacon').addClass("iAmFancy");
+  return $('p#bacon')
+}
+
+function addText(argument) {
+  $('p:last').append(argument)
+  return $('p:last')
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide()
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn(4000)
+}
+
+function fadeItOut() {
+  $('p:last').fadeOut(0)
+}
+
+function findItByClass(argument) {
+  return $(argument).hasClass("flatironLink")
+}
+
+function formValue() {
+  return $('input:last').val()
+}
