60bca7b9ba7233d17abef6b289e4f8cbaeab3e3a
gabe <gj+code@mail.co.de>
2016-09-21 20:19:24 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p[id!=bacon]').append(string);
+}
+
+function showDolphin() {
+  return $('img#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p[id!=bacon]').fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
