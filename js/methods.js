df20aa9766fc83ea9c45966f95482249879a6d82
phaltoe <reverse9@live.com>
2016-09-27 13:40:33 -0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,31 @@
-'use strict';
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy')
+}
+
+function addText(string) {
+  return $('p:nth-of-type(2)').append(string)
+}
+
+function showDolphin() {
+  $('img#hidden').show()
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide()
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn()
+}
+
+function fadeItOut() {
+  $('p:nth-of-type(2)').fadeOut(0)
+}
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink')
+}
+
+function formValue() {
+  return $('input:last').val()
+}
