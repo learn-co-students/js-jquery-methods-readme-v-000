7022f11137cbf0e27065d88a4f7ad509e25c7a0d
kaileighrose <kaileighrose@gmail.com>
2016-10-03 18:32:55 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,34 @@
 'use strict';
+function fancySchmancy() {
+  $('p#bacon').addClass('iAmFancy');
+  return $('p');
+}
+
+function addText(string) {
+  $('p:last').append(string);
+  return $('p:last');
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:last').fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
