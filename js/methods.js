d2fdc788307a26e3a0e4c5210043eec5d4fe8126
kaileeagray <kaileeagray@gmail.com>
2016-10-11 12:16:48 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  $("p").addClass("iAmFancy");
+  return $("p");
+ }
+
+ function addText(string) {
+   $('p:nth-of-type(2)').append(string);
+   return $('p:nth-of-type(2)');
+ }
+
+ function showDolphin() {
+   $('#hidden').show();
+ }
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn(2000);
+}
+
+function fadeItOut() {
+  $('p:nth-of-type(2)').fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
