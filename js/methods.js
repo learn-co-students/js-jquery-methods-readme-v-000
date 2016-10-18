dcc64bd72066f80572b8da913144cc3bc338bf9f
SiamKing <thesiameseelectric@gmail.com>
2016-10-18 11:47:48 -0700

    methods are cool, you are too

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function fancySchmancy() {
+    return $('p[id="bacon"]').addClass("iAmFancy");
+}
+
+function addText(string) {
+  return $("p:nth-of-type(2)").append(string);
+}
+ function showDolphin() {
+   $('img[id="hidden"]').show();
+ }
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('div[id="favorite-snacks"]').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:nth-of-type(2)').fadeOut(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
