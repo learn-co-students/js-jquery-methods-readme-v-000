39826b96744f7b7d0d78602a0e022de65c6d97dd
jchu4483 <jchu4483@gmail.com>
2016-10-19 21:41:10 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('#bacon').addClass('iAmFancy')
+}
+
+function addText(str) {
+  return $("p:nth-of-type(2)").append(str)
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $("p:nth-of-type(2)").fadeOut(0);
+}
+
+function findItByClass(str) {
+  return $(str).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('input:checkbox:last').val();
+}
