90230892e01ee6877f3f5b297d87ca21202a705d
Logan McAnsh <l.m@loganmcansh.com>
2016-10-25 17:47:04 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  $('p#bacon').addClass('iAmFancy');
+  return $('.iAmFancy');
+}
+
+function addText(string) {
+  $('p:first-child + p').append(string);
+  return $('p:first-child + p');
+}
+
+function showDolphin() {
+  $('#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:first-child + p').fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:checkbox:last').val();
+}
