589b882334ae800ee65d4de1167a2ddf41732555
Dom McKellar <dom.mckellar@gmail.com>
2016-11-05 10:46:14 -0700

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy(){
+  return $('#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p:nth-child(2)').append(string);
+}
+
+function showDolphin() {
+  return $("#hidden").show();
+}
+
+function hideImage() {
+  return $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  return $("#favorite-snacks").fadeIn();
+}
+
+function fadeItOut() {
+  return $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $("input:last").val();
+}
