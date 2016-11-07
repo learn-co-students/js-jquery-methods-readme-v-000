f18d04617858e1d31be9665471b4a67ffe980258
Caleb Faught <faughtcj@gmail.com>
2016-11-06 19:11:47 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p + p').append(string);
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn(1000);
+}
+
+function fadeItOut() {
+  $('p + p').fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
