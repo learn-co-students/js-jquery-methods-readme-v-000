6a8641204653dc793dbd70adc0dbd600e0401dd5
Traci7822 <Tracir@gmail.com>
2016-11-07 12:14:34 -0700

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy(){
+  return $("p#bacon").addClass("iAmFancy");
+}
+
+function addText(string){
+  return $("p:nth-child(2)").append(string);
+}
+
+function showDolphin(){
+  $('img#hidden').show();
+}
+
+function hideImage(){
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt(){
+  $("#favorite-snacks").fadeIn();
+}
+
+function fadeItOut(){
+  $("p:nth-child(2)").hide(0);
+}
+
+function findItByClass(selector){
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue(){
+  return $("input:last-child").val();
+}
