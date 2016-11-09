af129c9e403d470e4a17c17f7936c31ce77c858f
lenn hypolite <lennhydesign@gmail.com>
2016-11-09 03:45:48 -0500

    Done

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,34 @@
 'use strict';
+function fancySchmancy() {
+  return $("p#bacon").addClass("iAmFancy");
+}
+
+function addText(text) {
+  $("p:nth-of-type(2)").append(text);
+  return $("p:nth-of-type(2)");
+};
+
+function showDolphin(){
+  $("img#hidden").show();
+};
+
+function hideImage() {
+  $("img:nth-of-type(2)").hide();
+};
+
+function fadeIt() {
+  $("#favorite-snacks").fadeIn();
+};
+
+function fadeItOut() {
+  $("p:nth-of-type(2)").fadeOut(0);
+};
+
+function findItByClass(string) {
+  return $(string).hasClass('flatironLink');
+};
+
+function formValue () {
+   $('input:last').val();
+   return  $('input:last').val();
+};
