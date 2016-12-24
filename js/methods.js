ab6b6369af0097ffb39b95b67e6d6bd207035425
Clare <clare.e.horton@gmail.com>
2016-12-24 00:20:15 -0600

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  $("p#bacon").addClass("iAmFancy");
+  return $("p#bacon");
+}
+
+function addText(string) {
+  $("p:nth-child(2)").append(string);
+  return $("p:nth-child(2)");
+}
+
+function showDolphin() {
+  $("img#hidden").show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $("div#favorite-snacks").fadeIn(1000);
+}
+
+function fadeItOut() {
+  $("p:nth-child(2)").hide(0);
+}
+
+function findItByClass(string) {
+  return $(string).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $("input:last").val();
+}
