63e5b091840338dea8c019cfed32195def626129
AliceKB <alicekristinabalbuena@gmail.com>
2016-12-30 02:12:52 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $("p#bacon").addClass("iAmFancy");
+}
+
+function addText(string) {
+  return $("p:nth-child(2)").append(string);
+}
+
+function showDolphin() {
+  $("img#hidden").show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $("#favorite-snacks").fadeIn();
+}
+
+function fadeItOut() {
+  $("p:nth-child(2)").fadeOut(0)
+}
+
+function findItByClass(string) {
+  return $(string).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('input:last').val()
+}
