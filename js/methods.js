f7ff2cdc2c6070470e1a7b450299345efb31aa2a
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-01-03 10:52:16 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass("iAmFancy");
+};
+
+function addText(string) {
+  return $('p:last').append(string);
+};
+
+function showDolphin() {
+  $('img[alt="smiling dolphin 2"]').removeAttr('id')
+};
