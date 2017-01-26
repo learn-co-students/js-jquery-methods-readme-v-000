bfdf5768abe19a690869a6cad957fa34c361fcdd
github username <github email address>
2017-01-26 09:05:34 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,32 @@
 'use strict';
+function fancySchmancy() {
+  return $("#bacon").addClass("iAmFancy");
+}
+
+function addText(string) {
+  return $('p:nth-child(2)').append(string)
+}
+
+function showDolphin() {
+  $('img').show();
+}
+
+function hideImage() {
+  $('img').hide();
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val()
+}
