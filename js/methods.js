4348f3664b612ddb0e5e86fc47e7732e65e1bdee
ayafushimi <aya.fushimi@gmail.com>
2017-02-08 13:27:56 -0500

    Done

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy() {
+  $('p#bacon').addClass("iAmFancy");
+  return $('p#bacon');
+}
+
+function addText(str) {
+  $('p:first + p').append(str);
+  return $('p:first + p');
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn(4000);
+}
+
+function fadeItOut() {
+  $('p:first + p').fadeOut(0);
+}
+
+function findItByClass(str) {
+  return $(str).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $('input:last').val();
+}
