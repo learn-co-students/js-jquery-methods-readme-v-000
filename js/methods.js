54a56e58a6c54ceece2a3623a5b0cd913d5a8b1a
Christine Cua <cloudycua@gmail.com>
2019-02-05 22:32:18 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function fancySchmancy(){
+  $('#bacon').addClass('iAmFancy')
+  return $('.iAmFancy')
+}
+
+function addText(sentence){
+  $('p:first-child + p').append(sentence)
+  return $('p:first-child + p')
+}
+
+function showDophin(){
+  $('img[alt="smiling-dolphin 2"]').show()
+}
